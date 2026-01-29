f3d78df16cdded5dced8043586d1ef17b0168bbf
Jason <jason13.deng@gmail.com>
2026-01-29 10:40:43 +0900

    Setup HTML for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 // webpack.config.js
 import path from "node:path";
 
+// making sure our Webpack configuration has access to HtmlWebpackPlugin
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
 export default {
     mode: "development",
     entry: "./src/index.js",
@@ -9,4 +12,10 @@ export default {
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
     },
+    // then adding it as a plugin to the configuration object
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
 };
